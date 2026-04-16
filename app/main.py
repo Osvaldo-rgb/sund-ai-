912f5350aa8ade50c0ff36f9ba8d707cfdfaad17
Oficial Os <osvaldoc.migueloficial@gmail.com>
2026-04-16 19:26:38 +0100

    fix: permite CORS temporariamente para testes no Railway

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.get_cors_origins(),
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
